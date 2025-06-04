3e673cee1abbd5cdc9281b790d8499fa96934c9c
Josu Martinez <josu.martinez@thoughtworks.com>
2025-06-04 23:39:55 +0200

    refactor: optimise anagram group finder code

--- src/main/java/com/global/commtech/test/anagramfinder/AnagramGroupFinder.java
+++ src/main/java/com/global/commtech/test/anagramfinder/AnagramGroupFinder.java
@@ -14,20 +14,23 @@ public class AnagramGroupFinder {
     }
 
     private static Map<String, Set<String>> find(final List<String> remainingWords, final Map<String, Set<String>> anagramGroups) {
-        for (String word : remainingWords) {
-            boolean doesAnagramExist = false;
-            for (Entry<String, Set<String>> anagramGroup : anagramGroups.entrySet()) {
-                if (AnagramChecker.areAnagrams(word, anagramGroup.getKey())) {
-                    anagramGroup.getValue().add(word);
-                    doesAnagramExist = true;
-                    break;
-                }
-            }
-            if (!doesAnagramExist) {
-                anagramGroups.put(word, new HashSet<>(List.of(word)));
-            }
-            return find(remainingWords.subList(remainingWords.indexOf(word) + 1, remainingWords.size()), anagramGroups);
+        // Base case
+        if (remainingWords.isEmpty()) {
+            return anagramGroups;
         }
-        return anagramGroups;
+        // Recursive case
+        String word = remainingWords.get(0);
+        boolean doesAnagramGroupForCurrentWordExist = false;
+        for (Entry<String, Set<String>> anagramGroup : anagramGroups.entrySet()) {
+            if (AnagramChecker.areAnagrams(word, anagramGroup.getKey())) {
+                anagramGroup.getValue().add(word);
+                doesAnagramGroupForCurrentWordExist = true;
+                break;
+            }
+        }
+        if (!doesAnagramGroupForCurrentWordExist) {
+            anagramGroups.put(word, new HashSet<>(List.of(word)));
+        }
+        return find(remainingWords.subList(remainingWords.indexOf(word) + 1, remainingWords.size()), anagramGroups);
     }
 }
