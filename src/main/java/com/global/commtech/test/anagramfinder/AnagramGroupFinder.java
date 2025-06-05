5e283a04954804eb53f400ec67c4a6569c923d0a
Josu Martinez <josu.martinez@thoughtworks.com>
2025-06-05 09:08:42 +0200

    fix: convert anagram group finder into sequential process
    The former solution consisted in performing a concurrent call per word that iterates over all anagram groups so far created. This increases time-complexity and, eventually, blows the stack.
    
    The new solution is iterative and its complexity is linear, thus making it ideal to handle large datasets gracefully.

--- src/main/java/com/global/commtech/test/anagramfinder/AnagramGroupFinder.java
+++ src/main/java/com/global/commtech/test/anagramfinder/AnagramGroupFinder.java
@@ -1,36 +1,26 @@
 package com.global.commtech.test.anagramfinder;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.Map.Entry;
 
 public class AnagramGroupFinder {
     
     public static Map<String, Set<String>> find(final List<String> words) {
-        return find(words, new HashMap<>());
+        Map<String, Set<String>> anagramGroups = new HashMap<>();
+        for (String word : words) {
+            String canonicalForm = canonicalize(word);
+            anagramGroups.computeIfAbsent(canonicalForm, k -> new HashSet<>()).add(word);
+        }
+        return anagramGroups;
     }
 
-    private static Map<String, Set<String>> find(final List<String> remainingWords, final Map<String, Set<String>> anagramGroups) {
-        // Base case
-        if (remainingWords.isEmpty()) {
-            return anagramGroups;
-        }
-        // Recursive case
-        String word = remainingWords.get(0);
-        boolean doesAnagramGroupForCurrentWordExist = false;
-        for (Entry<String, Set<String>> anagramGroup : anagramGroups.entrySet()) {
-            if (AnagramChecker.areAnagrams(word, anagramGroup.getKey())) {
-                anagramGroup.getValue().add(word);
-                doesAnagramGroupForCurrentWordExist = true;
-                break;
-            }
-        }
-        if (!doesAnagramGroupForCurrentWordExist) {
-            anagramGroups.put(word, new HashSet<>(List.of(word)));
-        }
-        return find(remainingWords.subList(remainingWords.indexOf(word) + 1, remainingWords.size()), anagramGroups);
+    private static String canonicalize(String word) {
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 }
